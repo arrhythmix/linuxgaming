35e49b135a5b6c99dd16662b9a94478945d707e7
arrhythmix <arrhythmixmusic@gmail.com>
2018-01-03 07:11:15 -0800

    Update headlessminingoc.sh

--- overclocking/headlessminingoc.sh
+++ overclocking/headlessminingoc.sh
@@ -1,16 +1,19 @@
 !/usr/bin/bash 
 #Below are settings to adjust power delivery and enable settings needed for overclocks. 
+#Values for core and mem values may vary depending on bin.
+
 #enabled[1] or disable[0] Persistence mode:
 
-sudo nvidia-smi -pm 1
+#sudo nvidia-smi -pm 1
 
 #sets maximum voltage offset limit allowed, unqualified values will return maximum value allowed. 
-#sets voltage overclock and offset measured in microvolts [1000 microvolts = 1millivolt = 0.01 volts].
+#sets voltage overclock and offset measured in microvolts [1000 microvolts = 1 millivolt = 0.01 volts].
 #If unqualified value is given, maximum value allowed is returned.
-sudo nvidia-settings -a '[gpu:0]/GPUOverVoltageOffset=50000' sudo nvidia-settings -a GPUOverVoltageOffset=50000 
+
+#sudo nvidia-settings -a '[gpu:0]/GPUOverVoltageOffset=50000' sudo nvidia-settings -a GPUOverVoltageOffset=50000 
 
 #sets "PowerMizerMode" where 1 = "Prefer Maximum Performance 2 = "Adaptive" 3 = "Auto"
-sudo nvidia-settings -a '[gpu:0]/GPUPowerMizerMode=1'
+#sudo nvidia-settings -a '[gpu:0]/GPUPowerMizerMode=1'
 
 #Note the [3] after "Offset" Represents Level P3 performance. Which is the performance level of any card reach under maximum load. P2 or [2] could also be adjusted, but generally the GPU will scale itself accordingly to the
 #demands of the program. P1 or [1] could also be adjusted, but no required. P0 or [0] is idle clocks, leave this alone. sets Core Clock Offset. 110 is the maximum allowed on my card before artifacts, your values may vary.
